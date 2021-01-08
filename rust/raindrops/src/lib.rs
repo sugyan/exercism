a2dceb8509d23c3db6b086196e0ead469bf4956b
sugyan <sugi1982@gmail.com>
2021-01-08 23:14:13 +0900

    Add rust/raindrops

---
+++ rust/raindrops/src/lib.rs
@@ -0,0 +1,16 @@
+pub fn raindrops(n: u32) -> String {
+    let mut ret = String::new();
+    if n % 3 == 0 {
+        ret += "Pling";
+    }
+    if n % 5 == 0 {
+        ret += "Plang";
+    }
+    if n % 7 == 0 {
+        ret += "Plong";
+    }
+    if ret.is_empty() {
+        ret += &n.to_string();
+    }
+    ret
+}
