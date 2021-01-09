17ed3546da5b1c8c3cf3ebf5fb492fefa2cf093f
sugyan <sugi1982@gmail.com>
2021-01-10 01:01:28 +0900

    Improve rust/anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -2,18 +2,27 @@ use std::collections::{HashMap, HashSet};
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&str]) -> HashSet<&'a str> {
     let mut char_counts = HashMap::new();
-    word.to_lowercase()
+    let word_lower = word.to_lowercase();
+    word_lower
         .chars()
         .for_each(|c| *char_counts.entry(c).or_insert(0) += 1);
+    let is_anaglam = |w: &str| -> bool {
+        if w.len() != word.len() {
+            return false;
+        }
+        let w_lower = w.to_lowercase();
+        if w_lower == word_lower {
+            return false;
+        }
+        let mut counts = HashMap::new();
+        w_lower
+            .chars()
+            .for_each(|c| *counts.entry(c).or_insert(0) += 1);
+        counts == char_counts
+    };
     possible_anagrams
         .iter()
-        .filter(|&w| {
-            let mut counts = HashMap::new();
-            w.to_lowercase()
-                .chars()
-                .for_each(|c| *counts.entry(c).or_insert(0) += 1);
-            counts == char_counts && w.to_lowercase() != word.to_lowercase()
-        })
-        .map(|&s| s)
+        .filter(|&w| is_anaglam(&w))
+        .cloned()
         .collect()
 }
