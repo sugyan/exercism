7ecd6369acd281f1ed2a302a710a5077885725e1
sugyan <sugi1982@gmail.com>
2021-01-13 23:31:58 +0900

    Update rust/space-age

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -15,58 +15,25 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    fn earth_years() -> f64;
-    fn years_during(d: &Duration) -> f64 {
-        d.seconds as f64 / 31557600.0 / Self::earth_years()
-    }
+    fn years_during(d: &Duration) -> f64;
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! impl_planet {
+    ($planet:ident, $orbital_period:expr) => {
+        pub struct $planet;
+        impl Planet for $planet {
+            fn years_during(d: &Duration) -> f64 {
+                d.seconds as f64 / 31557600.0 / $orbital_period
+            }
+        }
+    };
+}
 
-impl Planet for Mercury {
-    fn earth_years() -> f64 {
-        0.2408467
-    }
-}
-impl Planet for Venus {
-    fn earth_years() -> f64 {
-        0.61519726
-    }
-}
-impl Planet for Earth {
-    fn earth_years() -> f64 {
-        1.0
-    }
-}
-impl Planet for Mars {
-    fn earth_years() -> f64 {
-        1.8808158
-    }
-}
-impl Planet for Jupiter {
-    fn earth_years() -> f64 {
-        11.862615
-    }
-}
-impl Planet for Saturn {
-    fn earth_years() -> f64 {
-        29.447498
-    }
-}
-impl Planet for Uranus {
-    fn earth_years() -> f64 {
-        84.016846
-    }
-}
-impl Planet for Neptune {
-    fn earth_years() -> f64 {
-        164.79132
-    }
-}
+impl_planet!(Mercury, 0.2408467);
+impl_planet!(Venus, 0.61519726);
+impl_planet!(Earth, 1.0);
+impl_planet!(Mars, 1.8808158);
+impl_planet!(Jupiter, 11.862615);
+impl_planet!(Saturn, 29.447498);
+impl_planet!(Uranus, 84.016846);
+impl_planet!(Neptune, 164.79132);
