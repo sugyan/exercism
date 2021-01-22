18a7232c4c659002bcd27557647b6586a1ab3493
sugyan <sugi1982@gmail.com>
2021-01-22 22:34:32 +0900

    Update rust/luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,21 +1,13 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let mut v = Vec::new();
-    for &b in code.as_bytes() {
-        match b {
-            b'0'..=b'9' => v.push(b - b'0'),
-            b' ' => {}
-            _ => return false,
-        }
-    }
-    if v.len() <= 1 {
-        return false;
-    }
-    v.iter()
+    code.chars()
+        .filter(|&c| c != ' ')
         .rev()
         .enumerate()
-        .map(|(i, &u)| if i % 2 == 0 { u } else { (u * 2) % 10 + u / 5 })
-        .sum::<u8>()
-        % 10
-        == 0
+        .try_fold((0, 0), |(sum, len), (i, c)| {
+            c.to_digit(10)
+                .map(|u| if i % 2 == 0 { u } else { (u * 2) % 10 + u / 5 })
+                .map(|u| (sum + u, len + 1))
+        })
+        .map_or(false, |(sum, len)| sum % 10 == 0 && len > 1)
 }
