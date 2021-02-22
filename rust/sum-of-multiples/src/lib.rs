ed09014e996869bf2452e60efa4240a8e0ddcaa0
sugyan <sugi1982@gmail.com>
2021-02-23 00:35:20 +0900

    Update rust/sum-of-multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,22 +1,5 @@
-use std::collections::HashSet;
-
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    let mut hs = HashSet::new();
-    helper(limit, factors, 1, &mut hs);
-    hs.iter().sum()
-}
-
-fn helper(limit: u32, factors: &[u32], start: u32, hs: &mut HashSet<u32>) {
-    if let Some(&factor) = factors.first() {
-        if factor == 0 {
-            return;
-        }
-        helper(limit, &factors[1..], start, hs);
-        for n in (1..=(limit - 1) / factor).map(|i| start * i * factor) {
-            if n < limit && !hs.contains(&n) {
-                hs.insert(n);
-                helper(limit, &factors[1..], n, hs);
-            }
-        }
-    }
+    (1..limit)
+        .filter(|&n| factors.iter().any(|&f| f != 0 && n % f == 0))
+        .sum()
 }
