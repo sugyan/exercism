9e2c617e0b754ff4c84145f2bc8d12123955f117
sugyan <sugi1982@gmail.com>
2021-02-26 01:05:17 +0900

    Update rust/prime-factors

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,27 +1,11 @@
 pub fn factors(n: u64) -> Vec<u64> {
     let mut n = n;
-    let mut primes = vec![2; 1];
     let mut ret = Vec::new();
-    loop {
-        let last = primes[primes.len() - 1];
-        if last * last > n {
-            break;
+    while n > 1 {
+        if let Some(factor) = (2..=n).find(|m| n % m == 0) {
+            ret.push(factor);
+            n /= factor;
         }
-        while n % last == 0 {
-            ret.push(last);
-            n /= last;
-        }
-        if let Some(prime) = (last + 1..).take_while(|&i| i * i <= n).find(|&i| {
-            primes
-                .iter()
-                .take_while(|&p| p * p <= i)
-                .all(|&p| i % p != 0)
-        }) {
-            primes.push(prime);
-        }
-    }
-    if n > 1 {
-        ret.push(n);
     }
     ret
 }
