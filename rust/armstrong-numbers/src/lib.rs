1e7cbefab20f1489c8bd692ad8fd338052d7d845
sugyan <sugi1982@gmail.com>
2021-02-26 12:37:11 +0900

    Update rust/armstrong-numbers

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,21 +1,9 @@
-use std::cmp::Ordering;
-
 pub fn is_armstrong_number(num: u32) -> bool {
-    let mut n = num;
-    let mut nums = Vec::new();
-    while n > 0 {
-        nums.push(n % 10);
-        n /= 10;
-    }
-    if nums.iter().sum::<u32>() == 1 {
-        return num == 1;
-    }
-    for i in 1.. {
-        match nums.iter().map(|&n| n.pow(i)).sum::<u32>().cmp(&num) {
-            Ordering::Less => {}
-            Ordering::Equal => return true,
-            Ordering::Greater => return false,
-        }
-    }
-    unreachable!()
+    let nums = num
+        .to_string()
+        .as_bytes()
+        .iter()
+        .map(|&b| (b - b'0') as u32)
+        .collect::<Vec<_>>();
+    nums.iter().map(|&n| n.pow(nums.len() as u32)).sum::<u32>() == num
 }
